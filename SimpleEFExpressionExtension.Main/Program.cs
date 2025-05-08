f55540bc9c47432a83971ec3e1de2460ff3fef2e
Семенов Вадим Сергеевич <Semenov.Vadim_S@russianpost.ru>
2025-05-08 14:41:59 +0300

    Add comments

--- SimpleEFExpressionExtension.Main/Program.cs
+++ SimpleEFExpressionExtension.Main/Program.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.Linq.Expressions;
+using System.Reflection;
 
-await using var dbContext = new SimpleDbContext();
+await using SimpleDbContext dbContext = new SimpleDbContext();
 
-var isDbCreated = await dbContext.Database.EnsureCreatedAsync();
+bool isDbCreated = await dbContext.Database.EnsureCreatedAsync();
 
 if (isDbCreated)
 {
@@ -12,27 +13,27 @@ if (isDbCreated)
 }
 
 //Фильтр по нескольким условиям, выбираются сущности для которых одно из условий верно. 
-var ordersWithOrConditions = dbContext.Orders
+List<Order> ordersWithOrConditions = dbContext.Orders
 	.Include(o => o.Customer)
 	.WhereOrConditions(o => o.Customer.FirstName == "John",
 		o => o.ProductName == "Onion")
 	.ToList();
 
 //Фильтр по нескольким условиям, выбираются сущности для которых все условия верны. 
-var ordersWithAndConditions = dbContext.Orders
+List<Order> ordersWithAndConditions = dbContext.Orders
 	.Include(o => o.Customer)
 	.WhereAndConditions(o => o.Customer.FirstName == "John",
 		o => o.ProductName == "Onion")
 	.ToList();
 
 //Фильтр по дате, в указанном диапазоне дат.
-var ordersInDateTimeRange = dbContext.Orders
+List<Order> ordersInDateTimeRange = dbContext.Orders
 	.WhereDateTimeBetween(x => x.DateTime, new DateTime(2025, 4, 25), DateTime.Now)
 	.ToList();
 
 //Фильтр по наличию подстроки в строковых свойствах
-var ordersWithPropertiesContainsText = dbContext.Orders
-	.Include(o=>o.Customer)
+List<Order> ordersWithPropertiesContainsText = dbContext.Orders
+	.Include(o => o.Customer)
 	.WhereAnyPropertyContainText("e",
 		o => o.Customer.FirstName,
 		o => o.ProductName)
@@ -42,59 +43,86 @@ Console.Read();
 
 public static class SimpleQueryableExtensions
 {
+	
+	// Метод фильтрует все свойства которые содержат текст 
 	public static IQueryable<T> WhereAnyPropertyContainText<T>(
 		this IQueryable<T> source,
-		string searchTerm,
+		string searchText,
 		params Expression<Func<T, string>>[] properties)
 	{
-		var parameter = Expression.Parameter(typeof(T), "x");
-		var containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+		//Это параметр, который будет использоваться во всех лямбдах массива properties, например x=>x.Customer.FirstName
+		//Имя параметра x
+		ParameterExpression parameter = Expression.Parameter(typeof(T), "x");
+		//Достаем метод Contains для вызова поиска текста
+		MethodInfo? containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) });
 		
 		if (containsMethod == null)
 			throw new InvalidOperationException("Contains method not found.");
 		
 		Expression body = Expression.Constant(false);
-		foreach (var property in properties)
+		//Проходим по всему массиву лямбд properties, и для каждого выражения
+		//проверяем содержание текста searchText через вызов метода Contains.
+		//Если выражение(свойство) содержит текст, то добавляем выражение к результирующему дереву выражений 
+		foreach (Expression<Func<T, string>> property in properties)
 		{
 			// Заменяем параметр исходного выражения на наш параметр
-			var propertyAccess = ReplaceAnotherParameter(property.Body, property.Parameters[0], parameter);
-			var containsCall = Expression.Call(propertyAccess, containsMethod, Expression.Constant(searchTerm));
+			// если имя параметр в лямбде не x, то меняем лямбду с o => o.ProductName на x => x.ProductName
+			// Имя параметра x задано выше в переменной parameter.
+			// Если не менять имена параметров в лямбдах, и они буду разные, то будет ошибка
+			Expression propertyAccess = ReplaceAnotherParameter(property.Body, property.Parameters[0], parameter);
+			MethodCallExpression containsCall = Expression.Call(propertyAccess, containsMethod, Expression.Constant(searchText));
 			body = Expression.OrElse(body, containsCall);
 		}
 		
-		var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
+		//Возвращаем результирующую лямбду
+		//На дебаге можно поставить точку останова и посмотреть ее
+		Expression<Func<T, bool>> lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
 		return source.Where(lambda);
 	}
 	
+	//Метод фильтрует по свойству типа DateTime, и выбирает все сущности во временном диапазоне
 	public static IQueryable<T> WhereDateTimeBetween<T>(this IQueryable<T> source,
-		Expression<Func<T, DateTime>> dateSelector, 
+		Expression<Func<T, DateTime>> dateSelector,
 		DateTime startDate, DateTime endDate)
 	{
-		var parameter = Expression.Parameter(typeof(T), "x");
-		var dateAccess = Expression.Invoke(dateSelector, parameter);
+		//Параметер используемый в лямбде
+		ParameterExpression parameter = Expression.Parameter(typeof(T), "x");
 		
-		var lowerBound = Expression.GreaterThanOrEqual(dateAccess, Expression.Constant(startDate));
-		var upperBound = Expression.LessThanOrEqual(dateAccess, Expression.Constant(endDate));
+		//Преобразуем делегат в аргумент(результирующую дату)
+		InvocationExpression dateAccess = Expression.Invoke(dateSelector, parameter);
 		
-		var body = Expression.AndAlso(lowerBound, upperBound);
+		//Сравниваем что дата больше startDate
+		BinaryExpression lowerBound = Expression.GreaterThanOrEqual(dateAccess, Expression.Constant(startDate));
+		//Сравниваем что дата меньше endDate
+		BinaryExpression upperBound = Expression.LessThanOrEqual(dateAccess, Expression.Constant(endDate));
 		
-		var lambda =  Expression.Lambda<Func<T, bool>>(body, parameter);
+		//Проводим операцию AND, если оба предыдущих выражения верны возвращаем True
+		BinaryExpression body = Expression.AndAlso(lowerBound, upperBound);
+		
+		//Возвращаем результирующую лямбду
+		//На дебаге можно поставить точку останова и посмотреть ее
+		Expression<Func<T, bool>> lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
 		
 		return source.Where(lambda);
 	}
 	
+	//Метод фильтрует сущности по массиву лямбд predicates.
+	//Если все лямбды возвращают true, то сущность добавляется в набор
 	public static IQueryable<T> WhereAndConditions<T>(this IQueryable<T> source,
 		params Expression<Func<T, bool>>[] predicates)
 	{
 		return source.WhereConditions(ConditionType.And, predicates);
 	}
 	
+	//Метод фильтрует сущности по массиву лямбд predicates.
+	//Если одна из лямбд возвращают true, то сущность добавляется в набор
 	public static IQueryable<T> WhereOrConditions<T>(this IQueryable<T> source,
 		params Expression<Func<T, bool>>[] predicates)
 	{
 		return source.WhereConditions(ConditionType.Or, predicates);
 	}
 	
+	//Метод фильтрует сущности по массиву лямбд predicates.
 	private static IQueryable<T> WhereConditions<T>(this IQueryable<T> source, ConditionType conditionType, params Expression<Func<T, bool>>[] predicates)
 	{
 		if (predicates.Length == 0)
@@ -103,10 +131,22 @@ public static class SimpleQueryableExtensions
 		if (predicates.Length == 1)
 			return source.Where(predicates[0]);
 		
-		var parameter = Expression.Parameter(typeof(T), "x");
-		var replacedPredicates = predicates.Select(p => ReplaceParameter(p, parameter)).ToList();
+		//Параметр используемый в лямбдах
+		ParameterExpression parameter = Expression.Parameter(typeof(T), "x");
 		
-		var body = replacedPredicates[0].Body;
+		// Заменяем параметр исходного выражения на наш параметр
+		// если имя параметр в лямбде не x, то меняем лямбду с o => o.ProductName на x => x.ProductName
+		// Имя параметра x задано выше в переменной parameter.
+		// Если не менять имена параметров в лямбдах, и они буду разные, то будет ошибка
+		List<Expression<Func<T, bool>>> replacedPredicates = predicates.Select(p =>
+		{
+			var newBody = ReplaceAnotherParameter(p.Body, p.Parameters[0], parameter);
+			return Expression.Lambda<Func<T, bool>>(newBody, parameter);
+		}).ToList();
+		
+		Expression body = replacedPredicates[0].Body;
+		//Проходим по всему массиву лямбд replacedPredicates, и для каждого выражения
+		//Строим новое дерево выражений body, в зависимости от условия добавляем сравнение AND или OR
 		for (int i = 1; i < replacedPredicates.Count; i++)
 		{
 			body = conditionType switch
@@ -117,38 +157,39 @@ public static class SimpleQueryableExtensions
 			};
 		}
 		
-		var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
+		//Возвращаем результирующую лямбду
+		//На дебаге можно поставить точку останова и посмотреть ее
+		Expression<Func<T, bool>> lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
 		
 		return source.Where(lambda);
 	}
 	
+	//Перечисление для изменения типа операции
 	private enum ConditionType
 	{
 		And,
 		Or
 	}
 	
+	//Вспомогательный класс для замены старого параметра на новый
 	private static Expression ReplaceAnotherParameter(
 		Expression expression,
 		ParameterExpression oldParameter,
 		ParameterExpression newParameter)
 	{
-		var visitor = new ParameterReplacerVisitor(oldParameter, newParameter);
+		ParameterReplacerVisitor visitor = new ParameterReplacerVisitor(oldParameter, newParameter);
 		return visitor.Visit(expression);
 	}
 	
-	private static Expression<Func<T, bool>> ReplaceParameter<T>(
-		Expression<Func<T, bool>> predicate,
-		ParameterExpression newParameter)
-	{
-		var visitor = new ParameterReplacerVisitor(predicate.Parameters[0], newParameter);
-		var newBody = visitor.Visit(predicate.Body);
-		return Expression.Lambda<Func<T, bool>>(newBody, newParameter);
-	}
-	
-	private class ParameterReplacerVisitor(ParameterExpression oldParameter,
+	//Класс наследуется от ExpressionVisitor
+	//ExpressionVisitor - это специальный класс, который позволяет пройти по всем узлам
+	//дерева выражений (ExpressionTree) и модифицировать их так как нам нужно.
+	private class ParameterReplacerVisitor(
+		ParameterExpression oldParameter,
 		ParameterExpression newParameter) : ExpressionVisitor
 	{
+		//Переопределяем базовый метод VisitParameter, и заменяем старый параметр в лямбде на новый
+		// то есть, если старый параметр x а новый y, то меняем лямбда поменяется с x=>x+1 на y=>y+1
 		protected override Expression VisitParameter(ParameterExpression node)
 		{
 			return node == oldParameter ? newParameter : base.VisitParameter(node);
@@ -171,21 +212,21 @@ public static class EntityCreator
 {
 	public static async Task AddEntities(this SimpleDbContext context)
 	{
-		var customer1 = new Customer
+		Customer customer1 = new Customer
 		{
 			FirstName = "John",
 			LastName = "Doe",
 			Age = 15
 		};
 		
-		var customer2 = new Customer
+		Customer customer2 = new Customer
 		{
 			FirstName = "Petr",
 			LastName = "Petrov",
 			Age = 30
 		};
 		
-		var customer3 = new Customer
+		Customer customer3 = new Customer
 		{
 			FirstName = "Pettr",
 			LastName = "Pettrov",
@@ -195,7 +236,7 @@ public static class EntityCreator
 		await context.Customers.AddRangeAsync(customer1, customer2, customer3);
 		await context.SaveChangesAsync();
 		
-		var order1 = new Order
+		Order order1 = new Order
 		{
 			OrderNumber = "1",
 			ProductName = "Tomato",
@@ -203,7 +244,7 @@ public static class EntityCreator
 			Customer = customer1
 		};
 		
-		var order2 = new Order
+		Order order2 = new Order
 		{
 			OrderNumber = "1",
 			ProductName = "Onion",
@@ -211,7 +252,7 @@ public static class EntityCreator
 			Customer = customer1
 		};
 		
-		var order3 = new Order
+		Order order3 = new Order
 		{
 			OrderNumber = "1",
 			ProductName = "Banana",
@@ -219,7 +260,7 @@ public static class EntityCreator
 			Customer = customer2
 		};
 		
-		var order4 = new Order
+		Order order4 = new Order
 		{
 			OrderNumber = "1",
 			ProductName = "Chery",
